6ec3bb758885198972b40d5ff5717a4dc4c5b5fa
BottaDev <fedebotta@gmail.com>
2021-05-11 22:54:03 -0300

    Added project

---
+++ Assets/Editor/MainWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MainWindow : EditorWindow
+{
+    [MenuItem("CustomTools/MapGenerator")]
+    public static void OpenWindow()
+    {
+        MainWindow window = GetWindow<MainWindow>();
+
+        window.wantsMouseMove = true;
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}
